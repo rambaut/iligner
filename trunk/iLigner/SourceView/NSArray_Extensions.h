63d0ce63fab7b08b270e9af8fb31cbfe5cf36496
rambaut <rambaut@b4ddeb3b-a02f-0410-b0e4-0b68386f80bd>
2008-07-03 22:30:13 +0000

    Experimenting

--- trunk/iLigner/SourceView/NSArray_Extensions.h
+++ trunk/iLigner/SourceView/NSArray_Extensions.h
@@ -1,7 +1,7 @@
 //
-// File:	   ImageAndTextCell.h
+// File:	   NSArray_Extensions.h
 //
-// Abstract:   Subclass of NSTextFieldCell which can display text and an image simultaneously.
+// Abstract:   Category extension to NSArray.
 //
 // Version:	   1.0
 //
@@ -45,18 +45,10 @@
 // Copyright (C) 2007 Apple Inc. All Rights Reserved.
 //
 
-#import <Cocoa/Cocoa.h>
+#import <Foundation/Foundation.h>
 
-@interface ImageAndTextCell : NSTextFieldCell
-{
-@private
-	NSImage *image;
-}
-
-- (void)setImage:(NSImage *)anImage;
-- (NSImage*)image;
-
-- (void)drawWithFrame:(NSRect)cellFrame inView:(NSView*)controlView;
-- (NSSize)cellSize;
-
+@interface NSArray (MyArrayExtensions)
+- (BOOL)containsObjectIdenticalTo:(id)object;
+- (BOOL)containsAnyObjectsIdenticalTo:(NSArray*)objects;
+- (NSIndexSet*)indexesOfObjects:(NSArray*)objects;
 @end
